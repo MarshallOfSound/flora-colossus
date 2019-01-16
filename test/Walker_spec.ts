f02f8bfe8f937d9067363ead43642445d98665db
Mark Lee <electronjs@lazymalevolence.com>
2019-01-16 15:53:29 -0800

    Add tests

--- test/Walker_spec.ts
+++ test/Walker_spec.ts
@@ -3,41 +3,67 @@ import { expect } from 'chai';
 
 import { Module, Walker } from '../src/Walker';
 import { DepType } from '../src/depTypes';
+import { NativeModuleType } from '../src/nativeModuleTypes';
+
+async function buildWalker(modulePath: string): Promise<Module[]> {
+  const walker = new Walker(modulePath);
+  return await walker.walkTree();
+}
 
 describe('Walker', () => {
-  let walker: Walker;
   let modules: Module[];
+  const thisPackageDir = path.resolve(__dirname, '..');
   const dep = (depName: string) => modules.find(module => module.name === depName);
 
-  beforeEach(async () => {
-    walker = new Walker(path.resolve(__dirname, '..'));
-     modules = await walker.walkTree();
-  });
-
   it('should save root directory correctly', () => {
-    expect(walker.getRootModule()).to.equal(path.resolve(__dirname, '..'));
+    const walker = new Walker(thisPackageDir)
+    expect(walker.getRootModule()).to.equal(thisPackageDir);
   });
 
-  it('should locate top level prod deps as prod deps', () => {
-    expect(dep('fs-extra')).to.have.property('depType', DepType.PROD);
+  describe('depType', () => {
+    beforeEach(async () => {
+      modules = await buildWalker(path.resolve(__dirname, '..'));
+    });
+
+    it('should locate top level prod deps as prod deps', () => {
+      expect(dep('fs-extra')).to.have.property('depType', DepType.PROD);
+    });
+
+    it('should locate top level dev deps as dev deps', () => {
+      expect(dep('mocha')).to.have.property('depType', DepType.DEV);
+    });
+
+    it('should locate a dep of a dev dep as a dev dep', () => {
+      expect(dep('commander')).to.have.property('depType', DepType.DEV);
+    });
+
+    it('should locate a dep of a dev dep that is also a top level prod dep as a prod dep', () => {
+      expect(dep('debug')).to.have.property('depType', DepType.PROD);
+    });
+
+    it('should locate a dep of a dev dep that is optional as a dev_optional dep', function () {
+      if (process.platform !== 'darwin') {
+        this.skip();
+      }
+      expect(dep('fsevents')).to.have.property('depType', DepType.DEV_OPTIONAL);
+    });
   });
 
-  it('should locate top level dev deps as dev deps', () => {
-    expect(dep('mocha')).to.have.property('depType', DepType.DEV);
-  });
+  describe('nativeModuleType', () => {
+    beforeEach(async () => {
+      modules = await buildWalker(path.join(__dirname, 'fixtures', 'native_modules'));
+    });
 
-  it('should locate a dep of a dev dep as a dev dep', () => {
-    expect(dep('commander')).to.have.property('depType', DepType.DEV);
-  });
+    it('should detect a module that uses prebuild', () => {
+      expect(dep('native-uses-prebuild')).to.have.property('nativeModuleType', NativeModuleType.PREBUILD);
+    });
 
-  it('should locate a dep of a dev dep that is also a top level prod dep as a prod dep', () => {
-    expect(dep('debug')).to.have.property('depType', DepType.PROD);
-  });
+    it('should detect a module that uses node-gyp', () => {
+      expect(dep('native-uses-node-gyp')).to.have.property('nativeModuleType', NativeModuleType.NODE_GYP);
+    });
 
-  it('should locate a dep of a dev dep that is optional as a dev_optional dep', function () {
-    if (process.platform !== 'darwin') {
-      this.skip();
-    }
-    expect(dep('fsevents')).to.have.property('depType', DepType.DEV_OPTIONAL);
+    it('should detect a module that is not native', () => {
+      expect(dep('pure-javascript-module')).to.have.property('nativeModuleType', NativeModuleType.NONE);
+    });
   });
 });
